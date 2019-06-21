907f008a137ae1c6010e3bd823d7b36ecf2533d9
eddie <edwardobande36@gmail.com>
2019-06-21 15:22:31 +0100

    initial commit

---
+++ src/components/Buttons/SubmitButton.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+import colors from '../../assets/colors';
+import {TouchableHighlight, StyleSheet, Text} from 'react-native';
+
+
+export default class SubmitButton extends Component {
+
+  render(){
+    const {disabled, onPress, title, btnstyle, titlestyle} = this.props;
+    const opacityStyle = disabled ? 0.2 : null;
+    const style = btnstyle || styles.button;
+    const titstyle = titlestyle || styles.title;
+    
+    return(
+      <TouchableHighlight 
+        style = {[{opacity: opacityStyle}, style]}
+        disabled = {disabled}
+        onPress = {onPress}>
+        <Text style = {titstyle}>
+          {title}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+SubmitButton.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  onPress : PropTypes.func.isRequired,
+  style : PropTypes.object,
+  title : PropTypes.string.isRequired,
+
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '70%',
+    height: 45,
+    marginTop: 16,
+    backgroundColor: colors.green,
+    borderRadius: 60,
+  },
+
+  title: {
+    fontFamily: 'Montserrat-Bold',
+    color: colors.white,
+  },
+
+  icon: {
+    marginRight: -2,
+    marginTop: -2,
+  }
+})
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { Alert , SingleButtonAlert} from './Alert';
+import {SubmitButton} from './Buttons';
+import {InputField, InputFieldBorder}  from './TextInput';
+import {Preloader}  from './Preloader';
+import {DisplayText}  from './Text';
+import  {Icons} from './Icons/Icons';
+
+export {
+  Alert , 
+  SingleButtonAlert, 
+  SubmitButton, 
+  InputField, 
+  Preloader, 
+  DisplayText, 
+  Icons, 
+  InputFieldBorder
+} 
